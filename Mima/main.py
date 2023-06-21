56134ef471f7e1cbabf669a63accd4038b40b9a6
lolfuck <werner.sprey@gmail.com>
2023-06-21 22:34:38 +0200

    Removed dumb directories

--- Mima/main.py
+++
@@ -1,9 +0,0 @@
-for number in range(101):
-    if number % 3 == 0 and number % 5 == 0:
-        print("foobar")
-    elif number % 3 == 0:
-        print("foo")
-    elif number % 5 == 0:
-        print("bar")
-    else:
-        print(number)
--- vorg.nvim/lua/vorg/init.lua
+++
@@ -1,9 +0,0 @@
-function Get_filename()
-    return string.format("%s ", vim.fn.expand("%:t"))
-end
-
-local M = {}
-
-function M.helloWorld() print(Get_filename()) end
-
-return M
