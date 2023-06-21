cc291157ee69e6cbfa06b7b885d4827c190ba195
lolfuck <werner.sprey@gmail.com>
2023-06-21 18:22:05 +0200

    Ugly string comparisons and vim fugitive

--- quick/finished/blackjack.c
+++ quick/finished/blackjack.c
@@ -6,25 +6,25 @@ int main(int argc, char* argv[]) {
     int value = 0;
 
     for(int i = 0; i < 5; i++) {
-        if(array[i] == "10" || array[i] == "Q" || array[i] == "J" || array[i] == "K")
+        if(*array[i] == '10' || *array[i] == 'Q' || *array[i] == 'J' || *array[i] == 'K')
             value += 10;
-        else if(array[i] == "9")
+        else if(*array[i] == '9')
             value += 9;
-        else if(array[i] == "8")
+        else if(*array[i] == '8')
             value += 8;
-        else if(array[i] == "7")
+        else if(*array[i] == '7')
             value += 7;
-        else if(array[i] == "6")
+        else if(*array[i] == '6')
             value += 6;
-        else if(array[i] == "5")
+        else if(*array[i] == '5')
             value += 5;
-        else if(array[i] == "4")
+        else if(*array[i] == '4')
             value += 4;
-        else if(array[i] == "3")
+        else if(*array[i] == '3')
             value += 3;
-        else if(array[i] == "2")
+        else if(*array[i] == '2')
             value += 2;
-        else if(array[i] == "A")
+        else if(*array[i] == 'A')
             value += 1;
     }
 
