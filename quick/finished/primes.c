90490e1df83ae64a2432776853d26b778d1c588f
lolfuck <werner.sprey@gmail.com>
2023-06-21 21:36:52 +0200

    Updated primes.c to be more efficient

--- quick/finished/primes.c
+++ quick/finished/primes.c
@@ -8,7 +8,7 @@ int main(int argc, char* argv[]) {
 
     for(int i = 4; i < input; i++) {
         int isPrime = 1;
-        for(int j = 2; j < i; j++)
+        for(int j = 2; j < i / 2; j++)
             if(i % j == 0) {
                 isPrime = 0;
                 break;
