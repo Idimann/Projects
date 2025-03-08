087241392b25b35e2eb3af3f41e8b6ac9b999af7
lolfuck <werner.sprey@gmail.com>
2025-03-08 15:55:38 +0100

    Revert "Init"
    This reverts commit eeb9284d58d2a2c620b2146f94b991393f9c0546.

--- learning/haskell/main.hs
+++ learning/haskell/main.hs
@@ -1,3 +1,5 @@
+import qualified Data.Bool as Data
+
 comparison :: (Int -> Int -> Bool) -> [Int] -> Bool
 
 comparison fn [] = True
@@ -7,6 +9,7 @@ comparison fn [x1, x2, x3] = fn x1 x2 && fn x2 x3
 comparison fn (x1:x2:x3:xs) = comparison fn [x1, x2] && comparison fn [x2, x3]
 
 toString :: Bool -> String
+toString True = "True"
 toString False = "False"
 
--- main = putStr $ toString $ comparison (<) [1, 2, 2]
+main = putStr $ toString $ comparison (<) [1, 2, 2]
--- learning/js/edabit/addition.js
+++ learning/js/edabit/addition.js
@@ -1,5 +1,5 @@
 function add(first, second) {
-    return (+first) + (+second);
+    return (first + 0) + (second + 0);
 }
 
 // import readline module
